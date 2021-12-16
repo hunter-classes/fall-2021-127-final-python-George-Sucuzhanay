08fc52ccb2c79c88f9be592bf36108653aeeab79
George <georgesucuzhanay100@gmail.com>
2021-12-16 12:55:55 -0500

    final edits

--- answers.py
+++ answers.py
@@ -1,5 +1,5 @@
-# print("------------------- Question 1 ----------------")
-# print("The function isIncreasing loops through the list of nums and checks to see if nums[i] is greater than nums[i-1]. This is done in order to check that the list of nums increases continouslly, if so returns True. If it does not, it returns False.")
+print("------------------- Question 1 ----------------")
+print("The function isIncreasing loops through the list of nums and checks to see if nums[i] is greater than nums[i-1]. This is done in order to check that the list of nums increases continouslly, if so returns True. If it does not, it returns False.")
 
 def isIncreasing(nums):
     next = nums[0]
@@ -13,10 +13,10 @@ def isIncreasing(nums):
 
     return True
 
-# print(isIncreasing([1,2,3,4,0]))
+print(isIncreasing([1,2,3,4,0]))
 
-# print("------------------- Question 2 ----------------")
-# print("This function loops through the list of nums and adds the converted str and adds it to the variable final. After loopign through all the nums in the list it return the an integer.")
+print("------------------- Question 2 ----------------")
+print("This function loops through the list of nums and adds the converted str and adds it to the variable final. After loopign through all the nums in the list it return the an integer.")
 
 def NumConvert(nums):
     final = "";
@@ -24,22 +24,22 @@ def NumConvert(nums):
         final = final + str(nums[i])
     return int(final)
 
-# print(NumConvert([1,2,3,4]))
+print(NumConvert([1,2,3,4]))
 # print(NumConvert([2,3,4,5,6,7]))
 
 
-# print("------------------- Question 3 ----------------")
-# print("This function loops in reverse order our binary string to a decimal. If it sees a 0 our deci is count + 3")
+print("------------------- Question 3 ----------------")
+print("This function loops in reverse order our binary string to a decimal. If it sees a 0 our deci is count + 3")
 
 def BinConvert(bin):
     reversed = ''
     for char in bin:
-        print(char)
         reversed = char + " " + reversed
     count = 0;
     a = reversed.split(" ")
     a1 = a[:-1]
-    print(a1, "wow")
+    print(a1, "str split into a list")
+    print()
     for i in range(len(a1)):
         if a1[i] == "0":
             count = count + 1
